b0a1406c93fb6f8e9f86aae9631ee4c26d17738c
Pascal Duez <pascal@duez.me>
2014-12-18 12:23:53 +0100

    Update tests

--- test/test.js
+++ test/test.js
@@ -31,19 +31,46 @@ cp.execFile('echo', ['hello'])
         console.log('[execFile] childProcess.pid: ', childProcess.pid);
     });
 
+var spawnOut = '';
 var spawnErr = '';
 
 cp.spawn('echo', ['hello'])
     .progress(function (childProcess) {
         console.log('[spawn] childProcess.pid: ', childProcess.pid);
+        childProcess.stdout.on('data', function (data) {
+            spawnOut += data;
+        });
         childProcess.stderr.on('data', function (data) {
             spawnErr += data;
         });
     })
-    .then(function (result) {
-        assert.equal(result.toString(), 'hello\n');
+    .then(function () {
+        assert.equal(spawnOut.toString(), 'hello\n');
         assert.equal(spawnErr.toString(), '');
     })
     .fail(function (err) {
         console.error("ERROR: ", (err.stack || err));
+    });
+
+cp.spawn('echo', ['hello'], { capture: ['stdout'] })
+    .progress(function (childProcess) {
+        console.log('[spawn] childProcess.pid: ', childProcess.pid);
+    })
+    .then(function (result) {
+        assert.equal(result.stdout.toString(), 'hello\n');
+    })
+    .fail(function (err) {
+        console.error("ERROR: ", (err.stack || err));
+    });
+
+cp.spawn('ls', ['./fail'], { capture: ['stdout', 'stderr'] })
+    .progress(function (childProcess) {
+        console.log('[spawn] childProcess.pid: ', childProcess.pid);
+    })
+    .then(function (result) {
+        assert.equal(result.stdout.toString(), '');
+    })
+    .fail(function (err) {
+        console.error("ERROR: ", (err.stack || err));
+        assert.equal(err.stderr.toString(), 'ls: ./fail: No such file or directory\n');
     });
