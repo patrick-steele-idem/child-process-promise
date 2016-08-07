850f8835d0640b339760d96260163458f4b6ee06
phil olson <philip.olson@protonmail.ch>
2016-08-07 10:17:15 -0500

    Enable es5: initial draft  - change main entry to ./index.js  - add logic to dynamically export the appropriate lib version and polyfill    'Promise' if necessary  - add prepublish script  - add node-version and promise-polyfill dependencies  - add necessary babel dev dependencies  - remove engine declaration in package.json  - add babel configuration

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+if (require('node-version').major >= 4) {
+  module.exports = require('./lib');
+} else {
+  global.Promise = require('promise-polyfill');
+  module.exports = require('./lib-es5');
+}
